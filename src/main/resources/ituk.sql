b794cfb5d307fd3512368a8d4f3756aea99164b6
Ilja Samoilov <ilsamoilov@gmail.com>
2019-09-22 00:37:24 +0300

    Added meeting participant table. Changed timestamps to timestamp with timezones. Updated foreign keys. Now all names are in singular form.

--- src/main/resources/ituk.sql
+++ src/main/resources/ituk.sql
@@ -3,20 +3,20 @@ create table door
     code varchar(255) primary key
 );
 
-create table resources
+create table resource
 (
     id         serial primary key,
     name       varchar(255),
     comment    text,
     url        varchar(255),
-    created_at timestamp not null,
-    updated_at timestamp not null,
+    created_at timestamptz default LOCALTIMESTAMP not null,
+    updated_at timestamptz default LOCALTIMESTAMP not null,
     author_id  integer
 );
 
 create table user_status
 (
-    status_id   serial       not null,
+    id          serial       not null,
     status_name varchar(255) not null,
     description varchar(255)
 );
@@ -31,37 +31,34 @@ create table "user"
     telegram     varchar(255),
     password     varchar(255),
     student_code varchar(255),
-    status_id    integer
-        constraint users_user_status_status_id_fk
-            references user_status
-            on update cascade on delete set null,
+    status_id    integer                            references user_status (id)
+                                                        on update cascade on delete set null,
     curriculum   varchar(255),
     iban         varchar(35),
     mentor_id    integer,
-    admin        boolean   default false          not null,
-    archived     boolean   default false          not null,
-    created_at   timestamp default LOCALTIMESTAMP not null,
-    updated_at   timestamp default LOCALTIMESTAMP not null
+    admin        boolean     default false          not null,
+    archived     boolean     default false          not null,
+    created_at   timestamptz default LOCALTIMESTAMP not null,
+    updated_at   timestamptz default LOCALTIMESTAMP not null
 );
 
 create table mentor
 (
-    user_id    integer                          not null
-        constraint mentors_users_id_fk
-            references "user",
-    curriculum varchar(255),
-    text       text,
-    gif        varchar(255),
-    quote      text,
-    enabled    boolean,
-    created_at timestamp default LOCALTIMESTAMP not null,
-    updated_at timestamp default LOCALTIMESTAMP not null
+    user_id      integer                            not null references "user" (id),
+    curriculum   varchar(255),
+    text         text,
+    gif          varchar(255),
+    quote        text,
+    enabled      boolean,
+    picture_name varchar(1000),
+    created_at   timestamptz default LOCALTIMESTAMP not null,
+    updated_at   timestamptz default LOCALTIMESTAMP not null
 );
 
 create unique index mentors_userid_uindex
     on mentor (user_id);
 
-create table applications
+create table application
 (
     id                    serial primary key,
     first_name            varchar(255),
@@ -71,30 +68,18 @@ create table applications
     student_code          varchar(255),
     curriculum            varchar(255),
     mentor_selection_code varchar(255),
-    created_at            timestamp default LOCALTIMESTAMP not null,
-    updated_at            timestamp default LOCALTIMESTAMP not null,
-    processed_by_id       integer
-        constraint processed_by_id
-            unique
-        constraint applications_users_id_fk
-            references "user",
-    mentor_id             integer
-        constraint mentor_id
-            unique
-        constraint applications_mentors_userid_fk
-            references mentor (user_id)
+    created_at            timestamptz default LOCALTIMESTAMP not null,
+    updated_at            timestamptz default LOCALTIMESTAMP not null,
+    processed_by_id       integer references "user" (id) on update cascade,
+    mentor_id             integer references mentor (user_id) on update cascade
 );
 
 
-create table door_permissions
+create table door_permission
 (
     id        serial primary key,
-    door_code varchar(255)
-        constraint door_permissions_door_code_fk
-            references door,
-    user_id   integer
-        constraint door_permissions_users_id_fk
-            references "user"
+    door_code varchar(255) references door (code),
+    user_id   integer references "user" (id)
 );
 
 
@@ -103,25 +88,18 @@ create table door_permission_log_entry
     id          serial primary key,
     updated_at  timestamp,
     change      text,
-    modified_by integer
-        constraint door_permission_log_entry_user_id_fk
-            references "user"
+    modified_by integer references "user" (id)
 );
 
 
 
-create table recovery_keys
+create table recovery_key
 (
     id         serial primary key,
     key        varchar(255),
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    user_id    integer
-        constraint user_id
-            unique
-        constraint recovery_keys_users_id_fk
-            references "user"
-            on update cascade
+    created_at timestamptz default LOCALTIMESTAMP not null,
+    updated_at timestamptz default LOCALTIMESTAMP not null,
+    user_id    integer unique references "user" (id) on update cascade
 );
 
 create table general_meeting
@@ -129,8 +107,16 @@ create table general_meeting
     id         serial primary key,
     name       varchar(255),
     date       timestamp,
-    created_at timestamp not null,
-    updated_at timestamp not null
+    created_at timestamptz default LOCALTIMESTAMP not null,
+    updated_at timestamptz default LOCALTIMESTAMP not null
+);
+
+create table meeting_participant
+(
+    user_id      integer references "user" (id),
+    meeting_id   integer references general_meeting (id),
+    participated boolean default false,
+    mandatory    boolean default false
 );
 
 create function trigger_set_timestamp() returns trigger
@@ -142,8 +128,6 @@ BEGIN
     RETURN NEW;
 END;
 $$;
-
-alter function trigger_set_timestamp() owner to ilja;
 
 create trigger updated_at
     before update
@@ -159,7 +143,7 @@ execute procedure trigger_set_timestamp();
 
 create trigger updated_at
     before update
-    on applications
+    on application
     for each row
 execute procedure trigger_set_timestamp();
 
@@ -171,7 +155,7 @@ execute procedure trigger_set_timestamp();
 
 create trigger updated_at
     before update
-    on recovery_keys
+    on recovery_key
     for each row
 execute procedure trigger_set_timestamp();
 
