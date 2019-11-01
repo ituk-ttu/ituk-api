2fa9a62da46152d2dcbd6621a89a76fb94c12e2a
jparve <jparve@ttu.ee>
2019-11-02 00:27:32 +0200

    added project budget and budget row

--- src/main/java/ee/ituk/api/project/domain/ProjectBudget.java
+++ src/main/java/ee/ituk/api/project/domain/ProjectBudget.java
@@ -1,4 +1,23 @@
 package ee.ituk.api.project.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "project_budget", schema = "public")
 public class ProjectBudget {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @OneToMany(mappedBy = "project_budget")
+    private List<ProjectBudgetRow> projectBudgetRows;
 }
--- src/main/java/ee/ituk/api/project/domain/ProjectBudgetRow.java
+++ src/main/java/ee/ituk/api/project/domain/ProjectBudgetRow.java
@@ -1,4 +1,26 @@
 package ee.ituk.api.project.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "project_budget_row", schema = "public")
 public class ProjectBudgetRow {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_budget_id")
+    private ProjectBudget projectBudget;
+
+    private String description;
+
+    private BigDecimal itukCost;
+
+    private BigDecimal facultyCost;
 }
