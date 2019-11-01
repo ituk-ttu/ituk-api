7b3d2f22c315d89295e805d2c9b2944f2c87b824
Raimond Lume <lume.raimond@gmail.com>
2019-11-01 23:41:41 +0200

    Project hibernate mapping

--- src/main/java/ee/ituk/api/project/domain/Project.java
+++ src/main/java/ee/ituk/api/project/domain/Project.java
@@ -1,4 +1,29 @@
 package ee.ituk.api.project.domain;
 
+import ee.ituk.api.user.domain.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "project", schema = "public")
 public class Project {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private LocalDate dateStart;
+
+    private LocalDate dateEnd;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User projectLead;
 }
