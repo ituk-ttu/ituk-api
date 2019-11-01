25fad5696726a26b94abe39c72aee124419f580d
TaaviAnsper <taaviansperr@gmail.com>
2019-11-02 00:23:38 +0200

    domain projectmember and projectsummary done

--- src/main/java/ee/ituk/api/project/domain/ProjectSummary.java
+++ src/main/java/ee/ituk/api/project/domain/ProjectSummary.java
@@ -1,4 +1,36 @@
 package ee.ituk.api.project.domain;
 
+import ee.ituk.api.user.domain.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="project_summary", schema = "public")
 public class ProjectSummary {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime createdAt;
+
+    private String positiveSummary;
+
+    private String negativeSummary;
+
+    @OneToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "confirmed_by")
+    private User confirmedBy;
+
 }
