f18ff432ba14f4697fb075e0652ca99bd3c5e66f
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-02 18:02:47 +0200

    Deleted insert

--- src/main/resources/db/local/R__inserts.sql
+++ src/main/resources/db/local/R__inserts.sql
@@ -1,4 +1,2 @@
 -- this migration file can be changed throughout the project
-INSERT  INTO "user" (first_name, last_name, email, card_number, password, student_code, status_id, curriculum, iban)
-VALUES ('Tux', 'Linus', 'tux@ituk.ee', NULL, '$2a$12$A7bDfgysWzX3MTewp37eaOIXfk5ErSG2Y3AlgLlRI4QwjJXJXvYVG', '199999IAPM', 1, 'IAPM', NULL);
 
