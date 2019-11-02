9dff728bb846de6de4d8508ef235bb010c118c65
Raimond Lume <lume.raimond@gmail.com>
2019-11-02 13:28:21 +0200

    correct wrong validation code

--- src/main/java/ee/ituk/api/project/validation/rules/project/HasRequiredFields.java
+++ src/main/java/ee/ituk/api/project/validation/rules/project/HasRequiredFields.java
@@ -7,14 +7,14 @@ import ee.ituk.api.project.domain.Project;
 import java.util.Collections;
 import java.util.List;
 
-import static ee.ituk.api.common.validation.ValidationUtil.PROJECT_MEMBER_NOT_VALID;
+import static ee.ituk.api.common.validation.ValidationUtil.PROJECT_MISSING_REQUIRED_FIELDS;
 import static java.util.Collections.emptyList;
 
 public class HasRequiredFields implements ValidationRule<Project> {
     @Override
     public List<ValidationError> apply(Project object) {
         if (object.getTitle() == null || object.getDateStart() == null) {
-            return Collections.singletonList(ValidationError.builder().code(PROJECT_MEMBER_NOT_VALID).build());
+            return Collections.singletonList(ValidationError.builder().code(PROJECT_MISSING_REQUIRED_FIELDS).build());
         }
         return emptyList();
     }
