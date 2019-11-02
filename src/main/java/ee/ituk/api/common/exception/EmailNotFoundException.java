d880eef80c17f2a7113ac61593151e3df339d2de
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-02 21:08:18 +0200

    changed exception code

--- src/main/java/ee/ituk/api/common/exception/EmailNotFoundException.java
+++ src/main/java/ee/ituk/api/common/exception/EmailNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "Object not found")
+@ResponseStatus(code= HttpStatus.UNAUTHORIZED, reason = "Object not found")
 public class EmailNotFoundException extends UsernameNotFoundException {
 
     public EmailNotFoundException() {
