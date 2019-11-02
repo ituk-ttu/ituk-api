2a8611e9ab24b3b061f1eb173223bf9d2325a345
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-02 14:12:16 +0200

    Fixed authentication

--- src/main/java/ee/ituk/api/common/exception/IncorrectPasswordException.java
+++ src/main/java/ee/ituk/api/common/exception/IncorrectPasswordException.java
@@ -3,6 +3,6 @@ package ee.ituk.api.common.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "Object not found")
+@ResponseStatus(code= HttpStatus.UNAUTHORIZED, reason = "Inccorect password")
 public class IncorrectPasswordException extends RuntimeException {
 }
