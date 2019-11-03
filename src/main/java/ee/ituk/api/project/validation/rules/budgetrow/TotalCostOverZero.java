ef323dcc1c8d2f940306b5df567a95718614144f
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-03 12:09:41 +0200

    Fixed merge errors

--- src/main/java/ee/ituk/api/project/validation/rules/budgetrow/TotalCostOverZero.java
+++ src/main/java/ee/ituk/api/project/validation/rules/budgetrow/TotalCostOverZero.java
@@ -1,6 +1,6 @@
 package ee.ituk.api.project.validation.rules.budgetrow;
 
-import ee.ituk.api.common.validation.ValidationError;
+import ee.ituk.api.common.exception.ErrorMessage;
 import ee.ituk.api.common.validation.ValidationRule;
 import ee.ituk.api.project.domain.ProjectBudgetRow;
 
@@ -12,12 +12,12 @@ import static java.util.Collections.singletonList;
 
 public class TotalCostOverZero implements ValidationRule<ProjectBudgetRow> {
     @Override
-    public List<ValidationError> apply(ProjectBudgetRow object) {
+    public List<ErrorMessage> apply(ProjectBudgetRow object) {
         // neither values can be negative, total cost must be over zero
         if (object.getFacultyCost().signum() != -1 && object.getItukCost().signum() != -1
                 && object.getItukCost().add(object.getFacultyCost()).signum() == 1) {
             return emptyList();
         }
-        return singletonList(ValidationError.builder().code(PROJECT_BUDGET_ROW_VALUES_NOT_VALID).build());
+        return singletonList(ErrorMessage.builder().code(PROJECT_BUDGET_ROW_VALUES_NOT_VALID).build());
     }
 }
