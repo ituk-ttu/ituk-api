a953a94419581cd52c3a92af03c94a6c0d882205
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-13 19:04:26 +0200

    added mapper and dto to general meeting

--- src/main/java/ee/ituk/api/meeting/GeneralMeetingController.java
+++ src/main/java/ee/ituk/api/meeting/GeneralMeetingController.java
@@ -1,37 +1,38 @@
 package ee.ituk.api.meeting;
 
-import ee.ituk.api.meeting.domain.GeneralMeeting;
+import ee.ituk.api.meeting.dto.GeneralMeetingDto;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
-@RequestMapping("/meeting")
+@RequestMapping("meeting")
 @RequiredArgsConstructor
 public class GeneralMeetingController {
 
     private final GeneralMeetingService meetingService;
 
-    @GetMapping("")
+    private final GeneralMeetingMapper mapper = Mappers.getMapper(GeneralMeetingMapper.class);
+
+    @GetMapping
     public ResponseEntity getAllMeetings() {
         return ok(meetingService.getAll());
     }
 
-    @PostMapping("")
-    public ResponseEntity createMeeting(@RequestBody GeneralMeeting meeting) {
-        return ok(meetingService.create(meeting));
+    @PostMapping
+    public ResponseEntity createMeeting(@RequestBody GeneralMeetingDto meetingDto) {
+        return ok(mapper.meetingToDto(meetingService.create(mapper.dtoToMeeting(meetingDto))));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity updateMeeting(@PathVariable Long id, @RequestBody GeneralMeeting meeting) {
-        return ok(meetingService.update(id, meeting));
+    @PutMapping("{id}")
+    public ResponseEntity updateMeeting(@PathVariable Long id, @RequestBody GeneralMeetingDto meetingDto) {
+        return ok(mapper.meetingToDto(meetingService.update(id, mapper.dtoToMeeting(meetingDto))));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity deleteMeeting(@PathVariable Long id) {
         meetingService.delete(id);
         return ResponseEntity.noContent().build();
