abdff98c23632e9dfce6f172727485ab2c0948e1
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-30 00:57:29 +0200

    added endpoint for application mentor setting

--- src/main/java/ee/ituk/api/join/service/ApplicationService.java
+++ src/main/java/ee/ituk/api/join/service/ApplicationService.java
@@ -3,12 +3,17 @@ package ee.ituk.api.join.service;
 import ee.ituk.api.common.exception.NotFoundException;
 import ee.ituk.api.join.domain.Application;
 import ee.ituk.api.join.repository.ApplicationRepository;
-import ee.ituk.api.mentor.MentorProfileService;
+import ee.ituk.api.mail.MailService;
 import ee.ituk.api.user.UserService;
+import ee.ituk.api.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+
+import static ee.ituk.api.common.validation.ValidationUtil.getNotFoundError;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,7 @@ public class ApplicationService {
 
     private final ApplicationRepository applicationRepository;
     private final UserService userService;
+    private final MailService mailService;
 
     public Application createApplication(Application application) {
         return saveApplication(application);
@@ -42,5 +48,20 @@ public class ApplicationService {
         application.setProcessedBy(userService.findUserById(application.getProcessedBy().getId()));
         application.setMentor(application.getMentor());
         return applicationRepository.save(application);
+    }
+
+    public void setMentorToApplication(Long applicationId, Long mentorId) {
+        if (Objects.nonNull(applicationId) && Objects.nonNull(mentorId)) {
+            Application application = findApplicationById(applicationId);
+            User mentor = userService.findUserById(mentorId);
+            if (!mentor.getRole().isCanBeMentor()) {
+                throw new NotFoundException(Collections.singletonList(getNotFoundError(User.class)));
+            }
+            application.setMentor(mentor);
+            Application saved = applicationRepository.save(application);
+            mailService.sendNewMinionEmail(saved);
+        } else {
+            throw new NotFoundException(Collections.singletonList(getNotFoundError(Application.class)));
+        }
     }
 }
