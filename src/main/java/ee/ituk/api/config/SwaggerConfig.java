9132b82ff7980d81f33f099671bda70b601623ad
Ilja Samoilov <ilsamoilov@gmail.com>
2019-11-30 14:44:21 +0200

    added security key possibility to swagger

--- src/main/java/ee/ituk/api/config/SwaggerConfig.java
+++ src/main/java/ee/ituk/api/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +22,12 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ee.ituk"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(securitySchemes());
+    }
+
+    private static List<? extends SecurityScheme> securitySchemes() {
+
+        return List.of(new ApiKey("Bearer", "Authorization", "header"));
     }
 }
