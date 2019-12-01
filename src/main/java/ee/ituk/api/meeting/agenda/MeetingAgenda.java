d88fbd6dbf25572b039edc7a597f0d6ea9260672
Kaspar Kivistik <kaspar.kivistik@monese.com>
2019-12-01 14:17:32 +0200

    ULDKOOSOLEK-6: initial db without ordering

---
+++ src/main/java/ee/ituk/api/meeting/agenda/MeetingAgenda.java
@@ -0,0 +1,32 @@
+package ee.ituk.api.meeting.agenda;
+
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLDeleteAll;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "meeting_agenda", schema = "public")
+@SQLDelete(sql = "UPDATE meeting_agenda SET deleted_at = now() WHERE id = ?")
+@SQLDeleteAll(sql = "UPDATE meeting_agenda SET deleted_at = NOW() WHERE 1 = 1")
+@Where(clause = "deleted_at IS null")
+public class MeetingAgenda {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "meetingAgenda", fetch = FetchType.LAZY)
+    private List<MeetingAgendaItem> items = new ArrayList<>();
+}
---
+++ src/main/java/ee/ituk/api/meeting/agenda/MeetingAgendaItem.java
@@ -0,0 +1,24 @@
+package ee.ituk.api.meeting.agenda;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "meeting_agenda_item", schema = "public")
+public class MeetingAgendaItem {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "meeting_agenda_id")
+    private MeetingAgenda meetingAgenda;
+    private String item;
+    private LocalDateTime createdAt;
+}
