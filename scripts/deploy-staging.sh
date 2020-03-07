be916c326105ebb9b2f521dfdde04068b7faf186
Kaspar Kivistik <kaspar.kivistik@monese.com>
2020-03-07 21:55:46 +0200

    secrets

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -15,11 +15,14 @@ cd ./build/libs || exit
 # define SSHPASS env variable that sshpass uses with -e argument
 export SSHPASS=$DEPLOY_PASS
 export DS_URL=$DS_URL
-export DS_USER=DS_USER
-export DS_PASSWORD=DS_PASSWORD
+export DS_USER=$DS_USER
+export DS_PASSWORD=$DS_PASSWORD
 
 # copy generated jar file to server
 rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" * $SSH_USERNAME@ituk.ee:/home/deploy/newHub/dev
 
 # restart server
+sshpass -e ssh $SSH_USERNAME@ituk.ee export DS_URL=$DS_URL
+sshpass -e ssh $SSH_USERNAME@ituk.ee export DS_USER=$DS_USER
+sshpass -e ssh $SSH_USERNAME@ituk.ee export DS_PASSWORD=$DS_PASSWORD
 sshpass -e ssh $SSH_USERNAME@ituk.ee sudo systemctl restart newHubDev.service
