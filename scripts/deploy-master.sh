64220b776a8b4b6a55c012d14a02d883e80e7245
Raimond Lume <lume.raimond@gmail.com>
2020-03-07 20:21:00 +0200

    Try new .travis.yml

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -6,7 +6,7 @@ echo "started master deploy script"
 export SSHPASS=$DEPLOY_PASS
 
 # copy generated jar file to server
-rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" ./build/lib/* $SSH_USERNAME@ituk.ee:/home/deploy/newHub/master
+rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" build/lib/* $SSH_USERNAME@ituk.ee:/home/deploy/newHub/master
 
 # restart server
 sshpass -e ssh $SSH_USERNAME@ituk.ee systemctl restart newHubMaster.service
