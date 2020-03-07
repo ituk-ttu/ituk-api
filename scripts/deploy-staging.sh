d9d195c223af85abeee50398ead437f5c1c0190d
Raimond Lume <lume.raimond@gmail.com>
2020-03-07 19:38:48 +0200

    Initial travis.yml files

---
+++ scripts/deploy-staging.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# define SSHPASS env variable that sshpass uses with -e argument
+export SSHPASS=$DEPLOY_PASS
+
+# copy generated jar file to server
+rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" ../build/lib/* $SSH_USERNAME@ituk.ee:/home/deploy/newHub/dev
+
+# restart server
+sshpass -e ssh $SSH_USERNAME@ituk.ee systemctl restart newHubDev.service
