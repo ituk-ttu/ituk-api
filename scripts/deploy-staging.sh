083c617cde6f306e15ca0ea30b0a3c6984116e44
Raimond Lume <lume.raimond@gmail.com>
2020-03-07 21:25:07 +0200

    Try new .travis.yml

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -8,13 +8,15 @@ echo "starting build from script"
 ./gradlew build
 
 echo "ls after script build"
-tree -r build
+ls -al
+
+cd ./build/libs || exit
 
 # define SSHPASS env variable that sshpass uses with -e argument
 export SSHPASS=$DEPLOY_PASS
 
 # copy generated jar file to server
-rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" ./build/lib/* $SSH_USERNAME@ituk.ee:/home/deploy/newHub/dev
+rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" * $SSH_USERNAME@ituk.ee:/home/deploy/newHub/dev
 
 # restart server
 sshpass -e ssh $SSH_USERNAME@ituk.ee systemctl restart newHubDev.service
