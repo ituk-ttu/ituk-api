46a1c37e42bdb9d87c9a988893058bd0b37edbec
Kaspar Kivistik <kaspar.kivistik@monese.com>
2020-03-08 14:39:27 +0200

    send mentor selection code after application creation

--- src/main/java/ee/ituk/api/join/service/ApplicationService.java
+++ src/main/java/ee/ituk/api/join/service/ApplicationService.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 import static ee.ituk.api.common.validation.ValidationUtil.checkForErrors;
 import static ee.ituk.api.common.validation.ValidationUtil.getNotFoundError;
@@ -53,7 +54,7 @@ public class ApplicationService {
         checkForErrors(validator.validateOnCreate(application));
         application.setCreatedAt(LocalDateTime.now());
         application.setUpdatedAt(LocalDateTime.now());
-
+        application.setMentorSelectionCode(UUID.randomUUID().toString().replace("-", ""));
         if (application.getMentor() == null || application.getMentor().getId() == null) {
             application.setMentor(null);
         }
