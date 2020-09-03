f60c7c1b2b87fde2281c67087fa18a884796b6ee
Ilja Samoilov <isamoilov@twilio.com>
2020-09-03 22:22:16 +0300

    Added new way to deploy

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -14,11 +14,8 @@ fi
 # change to folder with generated jar file
 cd ./build/libs || exit
 
-# define SSHPASS env variable that sshpass uses with -e argument
-export SSHPASS=$DEPLOY_PASS
-
 # copy generated jar file to server
-rsync -chavzP --rsh="sshpass -e ssh -l $SSH_USERNAME" * $SSH_USERNAME@ituk.ee:/home/deploy/newHub/master
+rsync -chavzP --rsh="ssh -l $SSH_USERNAME" * $SSH_USERNAME@ituk.ee:/home/deploy/newHub/master
 
 # restart service with new jar
-sshpass -e ssh $SSH_USERNAME@ituk.ee sudo systemctl restart newHubMaster.service
+ssh $SSH_USERNAME@ituk.ee sudo systemctl restart newHubMaster.service
