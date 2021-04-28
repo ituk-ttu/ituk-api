c5a02a6f839f12e92c9a5c38daa6dce66b6b3c5b
Kaspar Kivistik <kaspar.kivistik@monese.com>
2021-04-28 14:48:08 +0300

    Makes EmailConfig fields immutable

--- src/main/java/ee/ituk/api/mail/EmailConfig.java
+++ src/main/java/ee/ituk/api/mail/EmailConfig.java
@@ -1,32 +1,35 @@
 package ee.ituk.api.mail;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 @ConfigurationProperties("email")
-@RequiredArgsConstructor
-@AllArgsConstructor
+@ConstructorBinding
 public class EmailConfig {
 
-    @Setter private String domain;
-    @Setter private String apiKey;
-    @Setter private String fromName;
-    @Setter private String fromEmail;
+    private final String domain;
+    private final String apiKey;
+    private final String fromName;
+    private final String fromEmail;
+
+    public EmailConfig(String domain, String apiKey, String fromName, String fromEmail) {
+        this.domain = domain;
+        this.apiKey = apiKey;
+        this.fromName = fromName;
+        this.fromEmail = fromEmail;
+    }
 
     @Bean
     public MailgunConfiguration getMailgunConfig() {
         return (MailgunConfiguration) new MailgunConfiguration().domain(domain)
-                                                                .apiKey(apiKey)
-                                                                .from(fromName, fromEmail)
-                                                                .apiUrl("https://api.mailgun.net/v3");
+                .apiKey(apiKey)
+                .from(fromName, fromEmail)
+                .apiUrl("https://api.mailgun.net/v3");
     }
 
     @Bean
