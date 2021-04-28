519c82e50408d7baf35107e7558cf64ddfe64c2e
Kaspar Kivistik <kaspar.kivistik@monese.com>
2021-04-28 14:48:08 +0300

    Makes EmailConfig fields immutable

--- src/main/java/ee/ituk/api/ApiApplication.java
+++ src/main/java/ee/ituk/api/ApiApplication.java
@@ -2,12 +2,14 @@ package ee.ituk.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@ConfigurationPropertiesScan
 public class ApiApplication {
 
 	public static void main(String[] args) {
